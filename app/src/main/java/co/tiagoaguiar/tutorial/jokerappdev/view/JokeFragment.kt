8d48e503f51070ee3d7a7bc662dc9770b9f64fca
brunoConnect <bruno.oliveira@connectdf.com.br>
2023-04-19 17:37:58 -0300

    Reload e piadas da categoria

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/JokeFragment.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/JokeFragment.kt
@@ -5,14 +5,33 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import co.tiagoaguiar.tutorial.jokerappdev.R
+import co.tiagoaguiar.tutorial.jokerappdev.model.Joke
+import co.tiagoaguiar.tutorial.jokerappdev.presentation.JokePresenter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class JokeFragment : Fragment() {
 
     companion object {
         const val CATEGORY_KEY = "category"
+    }
+
+    private lateinit var progressBar: ProgressBar
+    private lateinit var textView: TextView
+    private lateinit var imageView: ImageView
+
+    private lateinit var presenter: JokePresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        presenter = JokePresenter(this)
     }
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
@@ -21,10 +40,35 @@ class JokeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val categoryName = arguments?.getString(CATEGORY_KEY)
+        val categoryName = arguments?.getString(CATEGORY_KEY)!!
 
         Log.d("teste", categoryName.toString())
 
         activity?.findViewById<Toolbar>(R.id.toobar)?.title = categoryName
+        progressBar = view.findViewById(R.id.progress_bar)
+        textView = view.findViewById(R.id.txt_joke)
+
+        view.findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
+            presenter.findBy(categoryName)
+        }
+
+        presenter.findBy(categoryName)
     }
+
+    fun showJoke(joke: Joke) {
+        textView.text = joke.text
+    }
+
+    fun showProgress(){
+        progressBar.visibility = View.VISIBLE
+    }
+
+    fun hideProgress(){
+        progressBar.visibility = View.GONE
+    }
+
+    fun showFailure(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
 }
