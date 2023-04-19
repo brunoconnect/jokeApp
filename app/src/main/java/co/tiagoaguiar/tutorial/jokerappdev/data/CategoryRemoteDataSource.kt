fab7ceaafe482ef44397205104ee8ecf44403aa0
brunoConnect <bruno.oliveira@connectdf.com.br>
2023-04-19 15:18:43 -0300

    http client com retrofit

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/data/CategoryRemoteDataSource.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/data/CategoryRemoteDataSource.kt
@@ -3,24 +3,33 @@ package co.tiagoaguiar.tutorial.jokerappdev.data
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class CategoryRemoteDataSource {
 
-    fun findAllCategories(callback: ListCategoryCallback){
-        Handler(Looper.getMainLooper()).postDelayed({
-            val response = arrayListOf(
-                "Categoria 1",
-                "Categoria 2",
-                "Categoria 3",
-                "Categoria 4"
-            )
+    fun findAllCategories(callback: ListCategoryCallback) {
+        HTTPClient.retrofit().create(ChuckNorrisAPI::class.java).findAllCategories()
+            .enqueue(object : Callback<List<String>> {
+                override fun onResponse(
+                    call: Call<List<String>>, response: Response<List<String>>
+                ) {
+                    if(response.isSuccessful) {
+                        val categories = response.body()
+                        callback.onSucess(categories ?: emptyList())
+                    } else {
+                        val error = response.errorBody()?.string()
+                        callback.onError(error ?: "Erro desconhecido")
+                    }
+                    callback.onComplete()
+                }
 
-            Log.i("testeinfo", response.toString())
+                override fun onFailure(call: Call<List<String>>, t: Throwable) {
+                    callback.onError(t.message ?: "Erro interno")
+                    callback.onComplete()
+                }
 
-            callback.onSucess(response)
-//            onError("FALHA NA CONEXÃO. TENTE NOVAMENTE MAIS TARDE.")
-
-            callback.onComplete()
-        }, 2000)
+            })
     }
 }
