e6e148e106703e0d384cd22ced4fe8f754e3aae2
brunoConnect <bruno.oliveira@connectdf.com.br>
2023-04-20 10:09:30 -0300

    download de images e cache

--- app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/JokeFragment.kt
+++ app/src/main/java/co/tiagoaguiar/tutorial/jokerappdev/view/JokeFragment.kt
@@ -15,6 +15,7 @@ import co.tiagoaguiar.tutorial.jokerappdev.R
 import co.tiagoaguiar.tutorial.jokerappdev.model.Joke
 import co.tiagoaguiar.tutorial.jokerappdev.presentation.JokePresenter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.squareup.picasso.Picasso
 
 class JokeFragment : Fragment() {
 
@@ -47,6 +48,7 @@ class JokeFragment : Fragment() {
         activity?.findViewById<Toolbar>(R.id.toobar)?.title = categoryName
         progressBar = view.findViewById(R.id.progress_bar)
         textView = view.findViewById(R.id.txt_joke)
+        imageView = view.findViewById(R.id.img_joke)
 
         view.findViewById<FloatingActionButton>(R.id.fab).setOnClickListener {
             presenter.findBy(categoryName)
@@ -57,6 +59,7 @@ class JokeFragment : Fragment() {
 
     fun showJoke(joke: Joke) {
         textView.text = joke.text
+        Picasso.get().load(joke.iconUrl).into(imageView)
     }
 
     fun showProgress(){
